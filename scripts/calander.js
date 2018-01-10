d152b431b8dc826a446e4e91bf188fa089f8fb70
Aaron <ya974397@gnspes.ca>
2018-01-10 00:18:57 -0400

    added

---
+++ scripts/calander.js
@@ -0,0 +1,5 @@
+if (window.innerWidth <= 500px) {
+	document.getElementById("GoogleCalander").src = 
+	"https://calendar.google.com/calendar/embed?showNav=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=300&amp;width=300&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=blueshoe19%40hotmail.com&amp;color=%231B887A&amp;ctz=America%2FHalifax"
+	
+}
